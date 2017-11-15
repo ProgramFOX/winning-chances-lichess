167abb9e5ee6be07b438f2e635acb80e2ce27155
ProgramFOX <programfox@hotmail.be>
2017-11-15 18:40:58 +0100

    Add Dataset type and Datapoint struct

--- lib.rs
+++ lib.rs
@@ -1,3 +1,18 @@
+use std::collections::HashMap;
+
+pub type Dataset = HashMap<u32, Datapoint>;
+
+pub struct Datapoint {
+    pub value: u32,
+    pub total: u32,
+}
+
+impl Datapoint {
+    pub fn percentage_value(&self) -> f64 {
+        (self.value as f64) / (self.total as f64)
+    }
+}
+
 pub fn calculate_from_files<'a, I>(files: I) -> String
 where
     I: IntoIterator<Item = &'a str>
