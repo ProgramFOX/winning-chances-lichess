f30f0b9ca0a1152bcc6e13648198d6e1bf0c4f84
ProgramFOX <programfox@hotmail.be>
2017-11-15 18:07:18 +0100

    Take file name inputs

--- main.rs
+++ main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main() {
-    
+    println!("Enter the file paths, comma-separated:");
+    let mut files = String::new();
+    io::stdin().read_line(&mut files)
+        .expect("Failed to read file paths.");
+    let files = files.trim().split(",");
 }
