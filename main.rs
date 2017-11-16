bc4f7bf0a6252b0c2102ea8e41832339de69fed3
ProgramFOX <programfox@hotmail.be>
2017-11-16 17:55:42 +0100

    Display calculation time

--- main.rs
+++ main.rs
@@ -1,10 +1,12 @@
 use std::io;
 
 extern crate winning_chances_core;
+extern crate time;
 
 use winning_chances_core::calculate_from_files;
 
 fn main() {
+    let start = time::PreciseTime::now();
     println!("Enter the file paths, comma-separated:");
     let mut files = String::new();
     io::stdin()
@@ -12,4 +14,6 @@ fn main() {
         .expect("Failed to read file paths.");
     let files = files.trim().split(",");
     calculate_from_files(files);
+    let duration = start.to(time::PreciseTime::now());
+    println!("Time: {} seconds", duration.num_seconds());
 }
